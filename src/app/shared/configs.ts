fc347f55fcf2006f9f290b3af1ac37f13cb94d82
Jim Crowley <jamescro@microsoft.com>
2017-10-16 14:36:47 -0700

    initial commit

---
+++ src/app/shared/configs.ts
@@ -0,0 +1,9 @@
+/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+export const Configs = {
+  appId: '4d0278f8-59ea-44c0-846e-65f0596eed63',
+  scope: 'Calendars.Read Files.Readwrite User.Read Mail.Send'
+};
